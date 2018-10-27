b7b60401ee0435ac22ac1b22c87d42c269156b67
WAFER\konay <konayuki0455@gmail.com>
2018-10-28 07:01:32 +0900

    fixed methods

--- core.py
+++ core.py
@@ -7,6 +7,15 @@ class Frac:
                 v = v.strout()
         ans = "\\frac{" + value[0] + "}{" + value[1] + "}"
         return ans
+    def setup(arg):
+        value[0] = arg
+    def setdown(arg):
+        value[1] = arg
+    def getup():
+        return value[0]
+    def getdown():
+        return value[1]
+
 
 class Equation:
     value = [" ", " "]
@@ -16,6 +25,14 @@ class Equation:
                 v = v.strout()
         ans = value[0] + " = " + value[1]
         return ans
+    def setleft(arg):
+        value[0] = arg
+    def setright(arg):
+        value[1] = arg
+    def getleft():
+        return value[0]
+    def getright():
+        return value[1]
 
 class String:
     value = ""
@@ -26,10 +43,10 @@ command = "Right: <, Left: >, frac: f, remove: r, number: num, if cursor_ is bef
 print("hello! new expression has just generated!")
 print("_=")
 expression = Equation()
-expression.value[0] = Frac()
-expression.value[0].value[0] = "a"
-expression.value[0].value[1] = "b"
-expression.value[1] = "c"
+expression.setleft(Frac())
+expression.getleft().setup() = "a"
+expression.getleft().setdown() = "b"
+expression.setright() = "c"
 print(expression.strout())
 print(command)
 while True:
