e76987144383b4ce37cc463dc634badc0668936f
WAFER\konay <konayuki0455@gmail.com>
2018-10-28 06:10:55 +0900

    core input

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-# 標準入力から読み取って出力
-import sys
-for line in sys.stdin:
-   print("--> " + line)
+print("hello! new expression has just generated!")
+while True:
+    command = input()
+    print(command)
