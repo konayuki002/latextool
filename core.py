e5d8733d6e583c22913c9c96d7617d6a0234c991
WAFER\konay <konayuki0455@gmail.com>
2018-10-28 09:19:53 +0900

    rm mark

--- core.py
+++ core.py
@@ -10,12 +10,8 @@ class Statement:
             else:
                 strvalue.append(v.strout())
         tempformat = self.strformat
-        print(self.strformat)
         for i in range(len(strvalue)):
-            print(tempformat)
             tempformat = tempformat.replace('#' + str(i), strvalue[i])
-            print(tempformat)
-        print(tempformat)
         return tempformat
     def setvalue(self, index, value):
         self.value[index] = value
@@ -36,11 +32,8 @@ class Frac(Statement):
 command = "Right: <, Left: >, frac: f, remove: r, number: num, if cursor_ is before other statement and make some statement, update the old statement"
 print("hello! new expression has just generated!")
 frac = Frac()
-print(frac.strformat)
 frac.setvalue(0,"a")
 frac.setvalue(1,"b")
-print(frac.strformat)
-print(frac.strout())
 expression = Equation()
 expression.setvalue(0,frac)
 expression.setvalue(1,"c")
